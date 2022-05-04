d2a36f12d0952837249561a00117fb5c030c3b96
NickfuryX <nosinicolo@gmail.com>
2022-05-04 15:53:59 +0200

    scuff

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <app-header/>
+    
+    
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+  },
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <img src="../assets/img/dc-logo.png" alt="logo-dc">
+    <nav>
+      <ul>
+        <li v-for="(item, index) in navItem" :key="index">{{item.nome}}</li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+  data(){
+    return{
+      navItem: [
+        {
+          nome: "CHARACTERS",
+          link: '#',
+          stato: false,
+        },
+        {
+          nome: "COMICS",
+          link: '#',
+          stato: true,
+        },
+        {
+          nome: "MOVIES",
+          link: '#',
+          stato: false,
+        },
+        {
+          nome: "TV",
+          link: '#',
+          stato: false,
+        },
+        {
+          nome: "GAMES",
+          link: '#',
+          stato: false,
+        },
+        {
+          nome: "COLLECTIBLES",
+          link: '#',
+          stato: false,
+        },
+        {
+          nome: "VIDEOS",
+          link: '#',
+          stato: false,
+        },
+        {
+          nome: "FANS",
+          link: '#',
+          stato: false,
+        },
+        {
+          nome: "NEWS",
+          link: '#',
+          stato: false,
+        },
+        {
+          nome: "SHOP",
+          link: '#',
+          stato: false,
+        },
+      ]
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss"></style>
